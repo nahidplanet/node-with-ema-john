870786667da7fa29d633724d59da22a4f6d20649
nahidplanet <nahidplanet.bd@gmail.com>
2022-05-20 21:16:22 +0600

    modal added

--- src/components/OrderItem/OrderItem.js
+++ src/components/OrderItem/OrderItem.js
@@ -5,15 +5,15 @@ const OrderItem = () => {
   return (
     <div>
 
-      <div class="grid grid-cols-[2fr,6fr,1fr] my-2 items-center mx-auto rounded-lg border shadow-md md:flex-row md:max-w-xl dark:border-gray-700 bg-[#FFE0B3] ">
+      <div className="grid grid-cols-[2fr,6fr,1fr] my-2 items-center mx-auto rounded-lg border shadow-md md:flex-row md:max-w-xl dark:border-gray-700 bg-[#FFE0B3] ">
         <div className="image p-2">
-          <img class="object-cover w-32 bg-black h-32 rounded-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://flowbite.com/docs/images/products/product-1.png" alt="" />
+          <img className="object-cover w-32 bg-black h-32 rounded-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://flowbite.com/docs/images/products/product-1.png" alt="" />
         </div>
         <div className="content">
-          <div class="flex flex-col justify-between p-2 leading-normal">
-            <h5 class="mb-1 text-md font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
-            <p class="mb-1 text-sm font-normal text-gray-700 ">Price: $12</p>
-            <p class="mb-2 text-sm font-normal text-gray-700 ">Quantity: 2</p>
+          <div className="flex flex-col justify-between p-2 leading-normal">
+            <h5 className="mb-1 text-md font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+            <p className="mb-1 text-sm font-normal text-gray-700 ">Price: $12</p>
+            <p className="mb-2 text-sm font-normal text-gray-700 ">Quantity: 2</p>
           </div>
         </div>
         <div className=" flex justify-center items-center bg-black w-10 h-10 border rounded-full cursor-pointer">
--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,10 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ShoppingCart = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modal, setModal] = useState(true);
+
   return (
-    <div className="order-1 sm:order-2 h-full bg-[#FFE0B3] sm:h-screen sm:mt-16 md:mt-0 py-7 px-6 md:sticky top-0 mb-10 md:mb-0 ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quas vero ex ullam debitis itaque ab corporis, sapiente voluptas aperiam vel, hic ipsum consequuntur architecto tempore nulla. Ratione deserunt, dicta sunt aperiam ipsum exercitationem veritatis repellat quisquam accusantium illum, earum nisi hic rem maiores ea consequuntur pariatur beatae reprehenderit! Exercitationem.
-      </div>
+    <div className='order-1 md:order-2   '>
+      <button className='hidden ' onClick={() => setModal(!modal)}></button>
+      {modal ?
+        <>
+          <button
+            className="block md:hidden bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+            onClick={() => setShowModal(true)}
+          >
+            Show Cart
+          </button>
+          {showModal ? (
+            <>
+              <div
+                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+              >
+                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                  {/*content*/}
+                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
+                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                      <h3 className="text-3xl font-semibold">
+                        Cart Details
+                      </h3>
+                      
+                    </div>
+                    {/*body*/}
+                    <div className="relative p-6 flex-auto">
+                      <div className="my-4 text-slate-500 text-lg leading-relaxed">
+                       <h1>Product</h1>
+                       <h1>Product</h1>
+                       <h1>Product</h1>
+                       <h1>Product</h1>
+                       <h1>Product</h1>
+                       <h1>Product</h1>
+                      </div>
+                    </div>
+
+                    {/*footer*/}
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+          ) :
+
+            <div className="hidden md:block order-1 sm:order-2 h-full bg-[#FFE0B3] sm:h-screen sm:mt-16 md:mt-0 py-7 px-6 md:sticky top-0 mb-10 md:mb-0  ">
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+              <h1>PRODUCT : </h1>
+            </div>
+          }
+
+        </>
+        :
+        null
+      }
+    </div>
   );
 };
 
