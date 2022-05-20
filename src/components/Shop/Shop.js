363d8b9df66fe250de1f1198fb4e9325c708a966
nahidplanet <nahidplanet.bd@gmail.com>
2022-05-20 19:07:55 +0600

    shop page design and responsive

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductCard from '../ProductCard/ProductCard';
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
 
 const Shop = () => {
@@ -6,7 +7,20 @@ const Shop = () => {
     <div className=' grid md:grid-cols-[4fr,1fr]'>
       <div className="order-2 md:order-1 ">
         <div className="container sm:p-6 md:my-16">
-
+          <div className='grid md:grid-cols-3 gap-5'>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          <ProductCard></ProductCard>
+          </div>
         </div>
       </div>
       <ShoppingCart></ShoppingCart>
