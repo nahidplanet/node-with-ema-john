1eb2d830e3b63da498c787260948c2d74e14b0bb
nahidplanet <nahidplanet.bd@gmail.com>
2022-05-20 17:31:44 +0600

    singin page design and responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Shop from './components/Shop/Shop';
+import ReviewItems from './components/ReviewItem/ReviewItem';
+import Inventory from './components/Inventory/Inventory';
+import SingIn from './components/SingIn/SingIn';
+import SingUp from './components/SingUp/SingUp';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/shop' element={<Shop></Shop>}></Route>
+        <Route path='/review' element={<ReviewItems></ReviewItems>}></Route>
+        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/singin' element={<SingIn></SingIn>}></Route>
+        <Route path='/singup' element={<SingUp></SingUp>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { MenuIcon ,XIcon} from '@heroicons/react/solid';
 import logo from "../../images/Logo.svg"
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
   const [navbarOpen,setNavbarOpen] = useState();
@@ -9,12 +10,12 @@ const Header = () => {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-[#0F172A] 600 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
+            <CustomLink
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
+              to={'/'}
             >
               <img className='w-20 md:w-auto' src={logo} alt="" />
-            </a>
+            </CustomLink>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -35,36 +36,36 @@ const Header = () => {
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
+                <CustomLink
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
+                  to={'/shop'}
                 >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Order</span>
-                </a>
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Shop</span>
+                </CustomLink>
               </li>
               <li className="nav-item">
-                <a
+                <CustomLink
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
+                  to={'/review'}
                 >
                   <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Order Review</span>
-                </a>
+                </CustomLink>
               </li>
               <li className="nav-item">
-                <a
+                <CustomLink
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
+                  to={'/inventory'}
                 >
                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Manage Inventory</span>
-                </a>
+                </CustomLink>
               </li>
               <li className="nav-item">
-                <a
+                <CustomLink
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
+                  to={'/singin'}
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Login</span>
-                </a>
+                </CustomLink>
               </li>
             </ul>
           </div>
